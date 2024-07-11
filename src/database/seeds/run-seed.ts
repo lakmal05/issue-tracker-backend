bb5492dab133dc8dabd9eeb1eeaed7d591426c50
lakmal05 <lakmal@webmotech.com>
2024-07-11 14:55:51 +0530

    permission

--- src/database/seeds/run-seed.ts
+++ src/database/seeds/run-seed.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { RoleSeedService } from './role/role-seed.service';
-import { SeedModule } from './seed.module';
-import { UserSeedService } from './user/user-seed.service';
+import { NestFactory } from "@nestjs/core";
+import { RoleSeedService } from "./role/role-seed.service";
+import { SeedModule } from "./seed.module";
+import { UserSeedService } from "./user/user-seed.service";
+import { PermissionSeedService } from "./permission/permission-seed.service";
 
 const runSeed = async () => {
   const app = await NestFactory.create(SeedModule);
@@ -9,6 +10,7 @@ const runSeed = async () => {
   // run
   await app.get(RoleSeedService).run();
   await app.get(UserSeedService).run();
+  await app.get(PermissionSeedService).run();
 
   await app.close();
 };
--- src/database/seeds/seed.module.ts
+++ src/database/seeds/seed.module.ts
@@ -1,24 +1,25 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import appConfig from 'src/config/app.config';
-import databaseConfig from 'src/database/config/database.config';
-import { DataSource, DataSourceOptions } from 'typeorm';
-import { TypeOrmConfigService } from '../typeorm-config.service';
-import { RoleSeedModule } from './role/role-seed.module';
-import { StatusSeedModule } from './status/status-seed.module';
-import { UserSeedModule } from './user/user-seed.module';
-
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import appConfig from "src/config/app.config";
+import databaseConfig from "src/database/config/database.config";
+import { DataSource, DataSourceOptions } from "typeorm";
+import { TypeOrmConfigService } from "../typeorm-config.service";
+import { RoleSeedModule } from "./role/role-seed.module";
+import { StatusSeedModule } from "./status/status-seed.module";
+import { UserSeedModule } from "./user/user-seed.module";
+import { PermissionSeedModule } from "./permission/permission-seed.module";
 
 @Module({
   imports: [
     RoleSeedModule,
     StatusSeedModule,
     UserSeedModule,
+    PermissionSeedModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [databaseConfig, appConfig],
-      envFilePath: ['.env'],
+      envFilePath: [".env"],
     }),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
