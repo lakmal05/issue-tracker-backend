a4a37e370f9ab6f2a5defab02d6fecfa5ecc6c7a
lakmal05 <lakmal@webmotech.com>
2024-07-11 14:56:02 +0530

    seed

--- src/common/seed-objects/permission.seed-data.ts
+++ src/common/seed-objects/permission.seed-data.ts
@@ -1,87 +1,25 @@
 export const PermissionsHierarchyObj = {
-  CATEGORY: {
+  ISSUE: {
     PARENT: {
-      PERMISSION_NAME: 'CATEGORY',
-      DESCRIPTION: 'Category',
+      PERMISSION_NAME: "ISSUE",
+      DESCRIPTION: "Issue",
     },
     CHILDREN: {
-      VIEW_CATEGORY: {
-        PERMISSION_NAME: 'VIEW_CATEGORY',
-        DESCRIPTION: 'View Information ',
+      VIEW_ISSUE: {
+        PERMISSION_NAME: "VIEW_ISSUE",
+        DESCRIPTION: "View",
       },
-      CREATE_CATEGORY: {
-        PERMISSION_NAME: 'CREATE_CATEGORY',
-        DESCRIPTION: 'Create',
+      CREATE_ISSUE: {
+        PERMISSION_NAME: "CREATE_ISSUE",
+        DESCRIPTION: "Create",
       },
-      UPDATE_CATEGORY: {
-        PERMISSION_NAME: 'UPDATE_CATEGORY',
-        DESCRIPTION: 'Update',
+      UPDATE_ISSUE: {
+        PERMISSION_NAME: "UPDATE_ISSUE",
+        DESCRIPTION: "Update ",
       },
-      DELETE_CATEGORY: {
-        PERMISSION_NAME: 'DELETE_CATEGORY',
-        DESCRIPTION: 'Delete',
-      },
-    },
-  },
-
-  STAFF: {
-    PARENT: {
-      PERMISSION_NAME: 'STAFF',
-      DESCRIPTION: 'Staff',
-    },
-    CHILDREN: {
-      VIEW_STAFF: {
-        PERMISSION_NAME: 'VIEW_STAFF',
-        DESCRIPTION: 'View Infomation',
-      },
-      CREATE_STAFF: {
-        PERMISSION_NAME: 'CREATE_STAFF',
-        DESCRIPTION: 'Create',
-      },
-      UPDATE_STAFF: {
-        PERMISSION_NAME: 'UPDATE_STAFF',
-        DESCRIPTION: 'Update',
-      },
-      DELETE_STAFF: {
-        PERMISSION_NAME: 'DELETE_STAFF',
-        DESCRIPTION: 'Delete',
-      },
-    },
-  },
-
-  CUSTOMER: {
-    PARENT: {
-      PERMISSION_NAME: 'CUSTOMER',
-      DESCRIPTION: 'Customer',
-    },
-    CHILDREN: {
-      VIEW_DASHBOARD: {
-        PERMISSION_NAME: 'VIEW_CUSTOMER',
-        DESCRIPTION: 'View Infomation',
-      },
-    },
-  },
-  PRODUCT: {
-    PARENT: {
-      PERMISSION_NAME: 'PRODUCT',
-      DESCRIPTION: 'Product',
-    },
-    CHILDREN: {
-      CREATE_PRODUCT: {
-        PERMISSION_NAME: 'CREATE_PRODUCT',
-        DESCRIPTION: 'Create',
-      },
-      UPDATE_PRODUCT: {
-        PERMISSION_NAME: 'UPDATE_PRODUCT',
-        DESCRIPTION: 'Update',
-      },
-      DELETE_PRODUCT: {
-        PERMISSION_NAME: 'DELETE_PRODUCT',
-        DESCRIPTION: 'Delete ',
-      },
-      VIEW_PRODUCT: {
-        PERMISSION_NAME: 'VIEW_PRODUCT',
-        DESCRIPTION: 'View',
+      DELETE_ISSUE: {
+        PERMISSION_NAME: "DELETE_ISSUE",
+        DESCRIPTION: "Delete",
       },
     },
   },
